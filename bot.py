ce0dc7e1a630345abc6dd353acbcf5386f015ba9
duaey <ahmettenr56@gmail.com>
2026-01-05 20:11:54 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -7,19 +7,19 @@ from static_ffmpeg import run
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 
-# ffmpeg ayari
+# ffmpeg setup
 static_ffmpeg.add_paths()
 try:
     FFMPEG_EXE = run.get_or_fetch_platform_executables_else_raise()[0]
 except:
     FFMPEG_EXE = "ffmpeg"
 
-# health check kismi (koyeb icin sart)
+# koyeb health check
 class HealthCheckHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Bot is alive")
+        self.wfile.write(b"Bot is online")
 
 threading.Thread(target=lambda: HTTPServer(('0.0.0.0', 8080), HealthCheckHandler).serve_forever(), daemon=True).start()
 
@@ -28,22 +28,20 @@ intents.message_content = True
 intents.voice_states = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-ytdl_opts = {'format': 'bestaudio/best', 'noplaylist': True, 'quiet': True, 'no_warnings': True}
-# kanka buradaki -reconnect ayarlari sesin kesilmemesini saglar
+ytdl_opts = {'format': 'bestaudio/best', 'noplaylist': True, 'quiet': True}
 ffmpeg_opts = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 @bot.event
 async def on_ready():
-    print(f'bot is online kanka: {bot.user}')
+    print(f'bot online kanka: {bot.user}')
 
 @bot.command()
 async def play(ctx, *, url):
     if not ctx.author.voice:
-        return await ctx.send("get in a voice channel first bro")
+        return await ctx.send("get in a voice channel")
     
     if not ctx.voice_client:
-        # kanka baglanirken self_deaf kapali olsun bazen o da sorun cikariyor
-        await ctx.author.voice.channel.connect(self_deaf=False)
+        await ctx.author.voice.channel.connect()
     
     if ctx.voice_client.is_playing():
         ctx.voice_client.stop()
@@ -52,16 +50,9 @@ async def play(ctx, *, url):
         with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info['url']
-            # kanka direkt pcm audio kullanarak kütüphane riskini azaltiyoruz
             source = discord.FFmpegPCMAudio(url2, executable=FFMPEG_EXE, **ffmpeg_opts)
             ctx.voice_client.play(source)
     
-    await ctx.send(f'now playing: **{info["title"]}**')
-
-@bot.command()
-async def stop(ctx):
-    if ctx.voice_client:
-        await ctx.voice_client.disconnect()
-        await ctx.send("bye bye")
+    await ctx.send(f'playing: **{info["title"]}**')
 
 bot.run(os.environ.get('TOKEN'))
